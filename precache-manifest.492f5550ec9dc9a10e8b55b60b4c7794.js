05762890604da10b8ab6b4ea390ae109248aff17
David Kalina <32883508+DavidK1999@users.noreply.github.com>
2020-08-13 20:22:33 -0600

    Updates

---
+++ precache-manifest.492f5550ec9dc9a10e8b55b60b4c7794.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e96e5e349237f116e7533aaa3e75e301",
+    "url": "/Send-Grid-Test-Analytics-Page/index.html"
+  },
+  {
+    "revision": "08eb92ab6ce318257c1f",
+    "url": "/Send-Grid-Test-Analytics-Page/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "108e165e3f811c0996e0",
+    "url": "/Send-Grid-Test-Analytics-Page/static/js/2.336722e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Send-Grid-Test-Analytics-Page/static/js/2.336722e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08eb92ab6ce318257c1f",
+    "url": "/Send-Grid-Test-Analytics-Page/static/js/main.94b628eb.chunk.js"
+  },
+  {
+    "revision": "4bb7f1b2147c504dcf02",
+    "url": "/Send-Grid-Test-Analytics-Page/static/js/runtime-main.97686897.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Send-Grid-Test-Analytics-Page/static/media/logo.5d5d9eef.svg"
+  }
+]);
